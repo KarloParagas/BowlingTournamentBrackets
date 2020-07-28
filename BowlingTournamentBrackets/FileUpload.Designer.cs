e9de6343a13a2f171484da45aa9b558cc92ae5f7
KarloParagas <karloparagas@gmail.com>
2020-07-27 21:29:23 -0700

    Implemented file upload

---
+++ BowlingTournamentBrackets/FileUpload.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace BowlingTournamentBrackets
+{
+    partial class FileUpload
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.UploadBtn = new System.Windows.Forms.Button();
+            this.ChooseFileBtn = new System.Windows.Forms.Button();
+            this.UploadTxt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Choose a text file to process:";
+            // 
+            // UploadBtn
+            // 
+            this.UploadBtn.Location = new System.Drawing.Point(303, 69);
+            this.UploadBtn.Name = "UploadBtn";
+            this.UploadBtn.Size = new System.Drawing.Size(75, 23);
+            this.UploadBtn.TabIndex = 1;
+            this.UploadBtn.Text = "Upload";
+            this.UploadBtn.UseVisualStyleBackColor = true;
+            this.UploadBtn.Click += new System.EventHandler(this.UploadBtn_Click);
+            // 
+            // ChooseFileBtn
+            // 
+            this.ChooseFileBtn.Location = new System.Drawing.Point(202, 69);
+            this.ChooseFileBtn.Name = "ChooseFileBtn";
+            this.ChooseFileBtn.Size = new System.Drawing.Size(75, 23);
+            this.ChooseFileBtn.TabIndex = 0;
+            this.ChooseFileBtn.Text = "Browse Files";
+            this.ChooseFileBtn.UseVisualStyleBackColor = true;
+            this.ChooseFileBtn.Click += new System.EventHandler(this.ChooseFileBtn_Click);
+            // 
+            // UploadTxt
+            // 
+            this.UploadTxt.Location = new System.Drawing.Point(161, 34);
+            this.UploadTxt.Name = "UploadTxt";
+            this.UploadTxt.Size = new System.Drawing.Size(260, 20);
+            this.UploadTxt.TabIndex = 4;
+            this.UploadTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // FileUpload
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(433, 104);
+            this.Controls.Add(this.UploadTxt);
+            this.Controls.Add(this.ChooseFileBtn);
+            this.Controls.Add(this.UploadBtn);
+            this.Controls.Add(this.label1);
+            this.Name = "FileUpload";
+            this.Text = "Upload File";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button UploadBtn;
+        private System.Windows.Forms.Button ChooseFileBtn;
+        private System.Windows.Forms.TextBox UploadTxt;
+    }
+}
---
+++ BowlingTournamentBrackets/FileUpload.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BowlingTournamentBrackets
+{
+    public partial class FileUpload : Form
+    {
+        public static string FilePath;
+
+        public FileUpload()
+        {
+            InitializeComponent();
+            UploadBtn.Enabled = false;
+            UploadTxt.ReadOnly = true;
+        }
+
+        private void ChooseFileBtn_Click(object sender, EventArgs e)
+        {
+            // Opens up the user's computer files so they can select
+            // a file to upload
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // If the user selects a text file
+                if (fileDialog.FileName.Substring(fileDialog.FileName.Length - 4).Contains(".txt")) 
+                {
+                    // Enable the upload button
+                    UploadBtn.Enabled = true;               
+                }
+
+                // Assign the file path to the FilePath field
+                FilePath = fileDialog.FileName;
+
+                // Display the path in the text box
+                UploadTxt.Text = fileDialog.FileName;
+            }
+        }
+
+        private void UploadBtn_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+        }
+    }
+}
